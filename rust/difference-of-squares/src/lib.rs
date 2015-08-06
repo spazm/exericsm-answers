6cd8567213dddd36e4d0e423fa3fe408c969e602
Andrew Grangaard <granny-github@ofb.net>
2015-08-06 16:25:46 -0700

    a recursive sum_of_squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -6,13 +6,18 @@ pub fn square_of_sum(i:u64) -> u64 {
     }
     acum * acum
 }
+
 pub fn sum_of_squares(i:u64) -> u64 {
-    let mut acum : u64= 0;
-    for k in (1 .. i + 1){
-        acum += k*k
+    if i == 1
+    {
+        1
     }
-    acum
+    else
+    {
+        i*i + sum_of_squares(i - 1)
+    }
 }
+
 pub fn difference(i:u64) -> u64 {
     square_of_sum(i) - sum_of_squares(i)
 }
