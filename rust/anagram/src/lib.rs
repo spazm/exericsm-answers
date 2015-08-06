972713085377cf141e460c97f8a9cf8271538c01
Andrew Grangaard <granny-github@ofb.net>
2015-08-06 15:38:17 -0700

    rust anagram: merge lc and lc_sort

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,38 +1,41 @@
 pub fn anagrams_for(s: &str, anagrams: &[&str]) -> Vec<String> {
-    let sorted_string = lc_sorted_str(s); // : String
-    let lowercase_s = to_lowercase(s); // : String
+    let sorted_string = to_lowercase(s, true); // : String
+    let lowercase_s = to_lowercase(s, false);  // : String
     let mut answers = Vec::new();
     for anagram in anagrams {
-        let sorted_anagram = lc_sorted_str(&anagram);
+        let sorted_anagram = to_lowercase(&anagram, true);
         if sorted_string == sorted_anagram {
-            if lowercase_s != to_lowercase(&anagram) {
+            if lowercase_s != to_lowercase(&anagram, false) {
                 answers.push(anagram.to_string()) }}
     }
     answers
 }
 
-fn lc_sorted_str(s: &str) -> String {
-    // lower case the string and then sort.
-
+fn to_lowercase(s: &str, sort: bool) -> String {
+    //! lower case and optionally sort the string
     // to_lowercase is unstable on Strings/collections, so manually
     // unpack the string to chars and lowercase each char
     //   str -> chars -> lowercase (collapse iterator of Some)
     //       -> into_iter -> collect to Vec<char>.
-    let mut chars: Vec<char> = s.chars().map(|c| c.to_lowercase()
-                                             .next()
-                                             .expect("should be char")).into_iter().collect();
-    chars.sort();  // sort works in-place on mutable vec
-    let lc_sorted_string: String = chars.into_iter().collect();
-    lc_sorted_string
-}
 
-fn to_lowercase(instr: &str) -> String {
-    // copied from jhod0 on exercism.io
-    // http://exercism.io/submissions/dcf0f37018d7470db5a5aab54ed04563
     // to_lowercase returns an iterator of valid lowercase characters,
     // we choose the first one with next()
-    instr.chars().map(|c| c.to_lowercase()
-                      .next()
-                      .expect("Should be char"))
-        .collect::<String>()
+    // initially copied from jhod0 on exercism.io
+    // http://exercism.io/submissions/dcf0f37018d7470db5a5aab54ed04563
+
+    // map string to a collection of lowercase chars
+    let lc_char_map = s.chars().map(|c| c.to_lowercase()
+                                    .next()
+                                    .expect("should be char"));
+    // -> core::iter::Map<core::str::Chars<_>
+
+    // sort if necessary
+    let lc_string: String = if sort {
+        let mut chars: Vec<char> = lc_char_map.into_iter().collect();
+        chars.sort();  // sort works in-place on mutable vec
+        chars.into_iter().collect()
+    } else {
+        lc_char_map.collect::<String>()
+    };
+    lc_string
 }
