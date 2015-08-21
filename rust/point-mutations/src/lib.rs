41237c489da4e875f506707b6a3b9114bd2e4c1b
Andrew Grangaard <granny-github@ofb.net>
2015-08-20 23:57:04 -0700

    rust point-mutations: complete, passes all tests
    `next()` returns an `Option`.  We collapse this with `expect()` as our two strings
    are expected to have the same length.

--- rust/point-mutations/src/lib.rs
+++ rust/point-mutations/src/lib.rs
@@ -1,5 +1,38 @@
+/// calculates the Hamming difference between two DNA strands
+///
+/// Hamming distance between two strings is the count of single point mutations
+/// between the two strings, where one character has been replaced with another.
+///
+/// The Hamming distance is only defined for sequences of equal length. Hence you
+/// may assume that only sequences of equal length will be passed to your hamming
+/// distance function.
+///
+/// Example:
+///    GAGCCTACTAACGGGAT
+///    CATCGTAATGACGGCCT
+///    ^ ^ ^  ^ ^    ^^
+///
+///    The Hamming distance between these two DNA strands is 7.
+///
+///    assert_eq!(hamming_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"), 7);
 pub fn hamming_distance(s1 : &str, s2: &str) -> u32
 {
+    let mut difference = 0;
     println!("s1: {},\ns2: {}", s1, s2);
-    0
+    let s1_chars = s1.chars();
+    let mut s2_chars = s2.chars();
+
+    for s1_char in s1_chars {
+        let s2_char = s2_chars.next().expect("second string should have as many chars as first");
+        if s1_char != s2_char {
+            difference += 1
+        }
+        println!("s1_char: {:?}, s2_char: {:?}, diff:{} ", s1_char, s2_char, difference)
+    }
+    difference
+}
+
+#[test]
+fn test_readme_example() {
+    assert_eq!(hamming_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"), 7);
 }
--- rust/point-mutations/tests/point-mutations.rs
+++ rust/point-mutations/tests/point-mutations.rs
@@ -6,25 +6,21 @@ fn test_no_difference_between_empty_strands() {
 }
 
 #[test]
-#[ignore]
 fn test_no_difference_between_identical_strands() {
     assert_eq!(dna::hamming_distance("GGACTGA", "GGACTGA"), 0);
 }
 
 #[test]
-#[ignore]
 fn test_complete_hamming_distance_in_small_strand() {
     assert_eq!(dna::hamming_distance("ACT", "GGA"), 3);
 }
 
 #[test]
-#[ignore]
 fn test_small_hamming_distance_in_the_middle_somewhere() {
     assert_eq!(dna::hamming_distance("GGACG", "GGTCG"), 1);
 }
 
 #[test]
-#[ignore]
 fn test_larger_distance() {
     assert_eq!(dna::hamming_distance("ACCAGGG", "ACTATGG"), 2);
 }
