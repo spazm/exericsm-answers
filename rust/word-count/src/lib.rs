1db9dcbccb72b5b22436c37c04b25e13d03c171b
Andrew Grangaard <granny-github@ofb.net>
2015-08-20 21:20:31 -0700

    rust word-count: stub out correct type signature and HashMap

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+/// Write a program that given a phrase can count the occurrences of each word in that phrase.
+///
+/// Caveats from tests:
+/// 1. numbers count as words
+/// 2. downcase all words
+/// 3. Vector output should be in same order as string (by first occurance of word)
+/// 4. Non-word/number chars as separator.
+
+use std::collections::HashMap;
+
+pub fn word_count(s: &str) -> HashMap<String, u32> {
+    let mut results : HashMap<String, u32> = HashMap::new();
+
+    results
 }
