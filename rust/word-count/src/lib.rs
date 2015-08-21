88be6273c5970362000608eaa6e075cacef41388
Andrew Grangaard <granny-github@ofb.net>
2015-08-20 22:15:32 -0700

    rust word-count: downcase words for counting.  All tests pass

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -4,6 +4,7 @@
 /// 1. numbers count as words
 /// 2. downcase all words
 /// 3. Vector output should be in same order as string (by first occurance of word)
+///    -- this seems to be handled by HashMap automatically.
 /// 4. Non-word/number chars as separator.
 
 use std::collections::HashMap;
@@ -11,16 +12,35 @@ use std::collections::HashMap;
 pub fn word_count(s: &str) -> HashMap<String, u32> {
     let mut word_count : HashMap<String, u32> = HashMap::new();
 
-//    break string of input into words on non-alphanumeric
+    // break string of input into words on non-alphanumeric
     for word in s.split(|c: char| !c.is_alphanumeric()) {
-        // if word is empty, continue
+        // skip empty words returned by split
         if word.len() == 0 { continue }
-        // will need to downcase word
-        println!("word: {:?}", word);
 
-        //entry() returns a mutable pointer to the hash value for a given key
-        let counter = word_count.entry(String::from(word)).or_insert(0);
+        let word = to_lowercase(word);  // now a String
+        // println!("word: {:?}", word);
+
+        // entry() returns a mutable pointer to the hash value for a given key
+        let counter = word_count.entry(word).or_insert(0);
         *counter += 1;
     }
     word_count
+}
+
+fn to_lowercase(s: &str) -> String {
+    // to_lowercase is unstable on Strings/collections, so manually
+    // unpack the string to chars and lowercase each char
+
+    // to_lowercase returns an iterator of valid lowercase characters,
+    // we choose the first one with next().  We are concerned only with
+    // a consistent mapping, not necessarily a fully correct one.
+    // initially copied from jhod0 on exercism.io
+    // http://exercism.io/submissions/dcf0f37018d7470db5a5aab54ed04563
+
+    // map string to a collection of lowercase chars
+    s.chars()
+        .map(|c| c.to_lowercase()
+             .next()
+             .expect("should be char"))
+        .collect::<String>()
 }
