191e108ea793fe6191e8afd01f4b683f71f8299c
Andrew Grangaard <granny-github@ofb.net>
2015-08-20 23:34:04 -0700

    rust point-mutations: initial files from exercism

---
+++ rust/point-mutations/tests/point-mutations.rs
@@ -0,0 +1,30 @@
+extern crate point_mutations as dna;
+
+#[test]
+fn test_no_difference_between_empty_strands() {
+    assert_eq!(dna::hamming_distance("", ""), 0);
+}
+
+#[test]
+#[ignore]
+fn test_no_difference_between_identical_strands() {
+    assert_eq!(dna::hamming_distance("GGACTGA", "GGACTGA"), 0);
+}
+
+#[test]
+#[ignore]
+fn test_complete_hamming_distance_in_small_strand() {
+    assert_eq!(dna::hamming_distance("ACT", "GGA"), 3);
+}
+
+#[test]
+#[ignore]
+fn test_small_hamming_distance_in_the_middle_somewhere() {
+    assert_eq!(dna::hamming_distance("GGACG", "GGTCG"), 1);
+}
+
+#[test]
+#[ignore]
+fn test_larger_distance() {
+    assert_eq!(dna::hamming_distance("ACCAGGG", "ACTATGG"), 2);
+}
