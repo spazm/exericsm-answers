f557d1faed86c5c0ebd830f9b708785ed8b6eb87
Andrew Grangaard <granny-github@ofb.net>
2015-08-21 23:10:45 -0700

    remove extraneous let in count().  Document fns

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 
-pub fn count(c: char, strand: &str) -> usize {
-    // fold
-    let count = strand
-        .chars()
-        .filter(|&x| x == c)
-        .fold(0, |acc, _| acc + 1);
+static DNA_BASES: &'static str = "ACGT";
 
-    count
-}
-
+/// Count occurences of each DNA base (A,C,G,T) in s.
 pub fn nucleotide_counts(s: &str) -> HashMap<char, usize> {
     let mut counts : HashMap<char, usize> = HashMap::new();
-    let bases = vec!('A', 'T', 'C', 'G');
-    for base in bases {
+    for base in DNA_BASES.chars() {
         counts.insert(base, count(base, s));
     }
 
     counts
+}
+
+/// Count occurences of `c` in `strand`
+pub fn count(c: char, strand: &str) -> usize {
+    strand
+        .chars()
+        .filter(|&x| x == c)
+        .fold(0, |acc, _| acc + 1)
 }
