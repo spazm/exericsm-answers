313c90b6214a69e0f2868e927073ebff35c9d83a
Andrew Grangaard <granny-github@ofb.net>
2015-08-26 16:15:30 -0700

    rust robot-name: all tests pass.
    * Added wrappers for random letter/digit char generation.
    * Is there a better way to build a string from a sequence of
      functions that return chars?

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,4 +1,25 @@
+extern crate rand;
+use rand::{thread_rng, Rng, ThreadRng};
+
 pub struct Robot {name: String}
+
+struct Random(ThreadRng);
+/// Inspired by `gen_ascii_chars` from `rand`
+impl Random {
+    pub fn letter(&mut self) -> char {
+        const LETTERS : &'static [u8] =
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                  abcdefghijklmnopqrstuvwxyz";
+        let c: char = *self.0.choose(LETTERS).unwrap() as char;
+        c
+
+    }
+    pub fn digit(&mut self) -> char {
+        const DIGITS : &'static [u8] = b"0123456789";
+        let c: char = *self.0.choose(DIGITS).unwrap() as char;
+        c
+    }
+}
 
 impl Robot {
     pub fn new() -> Robot {
@@ -9,11 +30,14 @@ impl Robot {
     /// Names should be 5 characters long, 2 letters followed by 3 numbers
     ///
     /// The provided test is broken, in that short names pass.
-
-
     fn random_name() -> String {
-        let name = "AB123".to_string();
-
+        let mut rng = Random(thread_rng());
+        let mut name: String = String::from("");
+        name.push(rng.letter());
+        name.push(rng.letter());
+        name.push(rng.digit());
+        name.push(rng.digit());
+        name.push(rng.digit());
         name
     }
 
