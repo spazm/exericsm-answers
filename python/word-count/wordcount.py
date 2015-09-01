3f89b15fb8b79878b7f31c2d5c3a33fdefdeaeee
Andrew Grangaard <granny-github@ofb.net>
2015-09-01 12:35:29 -0700

    python word-count: done. used a collections.Counter
    http://exercism.io/submissions/34713dda5383470f8fce3da2ce92a67d

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,3 +1,10 @@
+from collections import Counter
+
 def word_count(corpus):
     """ Count words in a corpus """
-    pass
+
+    count = Counter()
+    for word in corpus.split():
+        count[word] += 1
+
+    return count
