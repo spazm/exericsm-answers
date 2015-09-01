1046678b7aca1853b80c3af87348b6f55fbfe047
Andrew Grangaard <granny-github@ofb.net>
2015-08-31 22:08:43 -0700

    rust phone-number: use map() from Option in area_code fn
    * like fmap/functor in haskell
    
    http://exercism.io/submissions/19f525df945b404bb47f461f9766fddc

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -2,9 +2,7 @@
 /// as parsed from input string in 10 digit or 11 digit
 /// with leading 1.
 pub fn number(n: &str) -> Option<String> {
-    println!("n: {}", n);
     let n = n.chars().filter(|&c| c.is_digit(10)).collect::<String>();
-    println!("n: {}", n);
     match n.len() {
         10 => Some(n),
         11 if n.starts_with("1") => Some(n[1..].to_string()),
@@ -14,10 +12,7 @@ pub fn number(n: &str) -> Option<String> {
 
 /// The area code of a 10 digit US phone number is the first three digits
 pub fn area_code(n: &str) -> Option<String> {
-    match number(n) {
-        None => None,
-        Some(n) => Some(n[0..3].to_string()),
-    }
+    number(n).map(|n| n[0..3].to_string())
 }
 
 /// pretty print a US phone number in "(XXX) XXX-XXXX" format
