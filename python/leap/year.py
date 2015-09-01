043286cdc0b609c8ad28605f75f2e66f200f2171
Andrew Grangaard <granny-github@ofb.net>
2015-09-01 12:28:33 -0700

    python leap: finished.  Pretty happy with the has_factor() closure
    http://exercism.io/submissions/ea111a2d583f43029a13990f96916e4b

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,3 +1,12 @@
-
 def is_leap_year(year):
-    True
+    """
+    A year is a leap year if it is divisible by 4,
+    excepting years divisible by 100 unless they are
+    also divisible by 400.
+    """
+
+    def has_factor(f):
+        return year % f == 0
+
+    return (has_factor(4) and
+            (not has_factor(100) or has_factor(400)))
