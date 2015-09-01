996ec3010289274e044ebda949f129e71703b25e
Andrew Grangaard <granny-github@ofb.net>
2015-08-31 23:26:20 -0700

    rust hexadecimal: #2: forward loop
    * TODO: can we make this a fold?

--- rust/hexadecimal/src/lib.rs
+++ rust/hexadecimal/src/lib.rs
@@ -1,14 +1,14 @@
 /// Convert hex value from string to Option<u32> decimal
 pub fn hex_to_int(hex: &str) -> Option<u32> {
     // first attempt: manual loop backwards through chars
+    // second attempt: fold forwards, multiply sum by 16
     let mut value = 0;
-    let mut multiple = 1;
-    for c in hex.chars().rev() {
+    for c in hex.chars() {
+        value *= 16;
         match hex_digit(c) {
             None => return None,
-            Some(d) => value += d * multiple,
+            Some(d) => value += d,
         }
-        multiple *= 16;
     }
     Some(value)
 }
