ddbf44fd6cd3252a3571311f3bcd1348bfed214a
Andrew Grangaard <granny-github@ofb.net>
2015-08-31 23:27:11 -0700

    rust hexadecimal: #2: forward loop
    * TODO: can we make this a fold?

--- rust/hexadecimal/src/lib.rs
+++ rust/hexadecimal/src/lib.rs
@@ -5,10 +5,7 @@ pub fn hex_to_int(hex: &str) -> Option<u32> {
     let mut value = 0;
     for c in hex.chars() {
         value *= 16;
-        match hex_digit(c) {
-            None => return None,
-            Some(d) => value += d,
-        }
+        hex_digit(c).and_then(|d| value += d)
     }
     Some(value)
 }
