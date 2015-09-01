68fcf0feb81c66d7f080f19e6de28b2981bda99f
Andrew Grangaard <granny-github@ofb.net>
2015-08-31 23:00:21 -0700

    rust hexadecimal: initial working solution. explicit loop
    http://exercism.io/submissions/359199d5a4e249c5998033a20fb1287a

--- rust/hexadecimal/src/lib.rs
+++ rust/hexadecimal/src/lib.rs
@@ -1,3 +1,28 @@
+/// Convert hex value from string to Option<u32> decimal
 pub fn hex_to_int(hex: &str) -> Option<u32> {
+    // first attempt: manual loop backwards through chars
+    let mut value = 0;
+    let mut multiple = 1;
+    for c in hex.chars().rev() {
+        match hex_digit(c) {
+            None => return None,
+            Some(d) => value += d * multiple,
+        }
+        multiple *= 16;
+    }
+    Some(value)
+}
 
+fn hex_digit(c: char) -> Option<u32> {
+    match c {
+        _ if c >= '0' && c <= '9'
+            => Some(c as u32 - '0' as u32),
+        'a' | 'A' => Some(10),
+        'b' | 'B' => Some(11),
+        'c' | 'C' => Some(12),
+        'd' | 'D' => Some(13),
+        'e' | 'E' => Some(14),
+        'f' | 'F' => Some(15),
+        _ => None,
+    }
 }
