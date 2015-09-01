f88ed543a4d1d7e60d8662baa618d431ff7b0ec1
Andrew Grangaard <granny-github@ofb.net>
2015-09-01 12:47:09 -0700

    python rna-transcription: complete + Invalid_Sequence exception
    http://exercism.io/submissions/b06209043fc24d658eff1cb20cada27b

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,3 +1,24 @@
 def to_rna(dna_seq):
-    """ Transcribes a DNA sequence to it's RNA complement """
+    """
+    Transcribes a DNA sequence to it's RNA complement,
+    mapping G->C, C->G, T->A, A->U.
+
+    Invalid characters in the dna_sequence will cause
+    an Invalid_Sequence Exception
+    """
+    dna_to_rna = {'G': 'C',
+                  'C': 'G',
+                  'T': 'A',
+                  'A': 'U'}
+
+    output = ""
+    for c in dna_seq:
+        if c not in dna_to_rna:
+            raise Invalid_Sequence(c)
+        output += dna_to_rna[c]
+
+    return output
+
+
+class Invalid_Sequence(BaseException):
     pass
