ce3fc3a9a646a2f487fd56ecd231963018e92b60
Andrew Grangaard <granny-github@ofb.net>
2016-02-15 13:04:32 -0800

    rust beer-song: remove empty case, rely on unsigned int being >0
    http://exercism.io/submissions/d99722b6ac5d4080b3f6289b6340b6e0

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -12,11 +12,10 @@ pub fn sing(start: u32, end: u32) -> String {
 /// One verse of the 99 bottles of beer song
 pub fn verse(n: u32) -> String {
     match n {
-        n if n > 2 => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {m} bottles of beer on the wall.\n",n=n, m=n-1),
         2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
         1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
-        _ => unreachable!()
+        n => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {m} bottles of beer on the wall.\n",n=n, m=n-1),
     }
 }
 
